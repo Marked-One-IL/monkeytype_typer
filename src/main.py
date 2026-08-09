7df34472930998a170ca528181324e028789a8f4
Marked-One <strelokoneil@gmail.com>
2026-08-09 09:11:08 +0300

    Made the typing faster by doing less driver calls. Also fixed grammer in comments and made the program exit without closing the website.

--- src/main.py
+++ src/main.py
@@ -1,32 +1,34 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+import os
 
 driver = webdriver.Chrome()
 driver.get("https://monkeytype.com/")
 
-def get_current_word():
-    """
-    When getting the words list. Sometimes the list in the website changes, and the list we have does not.
-    To combat this, we get the first word possible which was not marked used.
-    """
+def type_current_word(element):
     global driver
-    word = driver.find_element(By.CSS_SELECTOR,".word:not([data-processed='true'])")
-    driver.execute_script("arguments[0].setAttribute('data-processed', 'true');", word)
-    return word.text
+
+    # When getting the word list, the website sometimes changes it,
+    # so the list we have may become outdated.
+    # To prevent this, we get the first word that has not been marked as used.
+    word = driver.execute_script(
+    """
+        word = document.querySelector(".word:not([used])");
+        word.setAttribute("used", "");
+        return word.textContent;
+    """)
+    element.send_keys(word + Keys.SPACE)
 
 def main():
-    element = driver.find_element(By.ID, "wordsInput")
-    
     try:
+        element = driver.find_element(By.ID, "wordsInput")
         while True:
-            element.send_keys(get_current_word())
-            element.send_keys(Keys.SPACE)              
+            type_current_word(element)
     except:
-        print("Typing stopped!")
-        
-    while driver.window_handles:
         pass
-        
+
+    os._exit(0) # Aggressive exit to bypass destructors and keep the website open.
+
 if __name__ == "__main__":
     main()
