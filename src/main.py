2626a932432cc5d83bfc7ff6aeda952100fb1ee6
Marked-One <strelokoneil@gmail.com>
2026-08-09 09:21:45 +0300

    Removed a func and made it inline.

--- src/main.py
+++ src/main.py
@@ -1,32 +1,25 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 import os
 
-driver = webdriver.Chrome()
-
-def type_current_word(input_element):
-    global driver
-
-    # When getting the word list, the website sometimes changes it,
-    # so the list we have may become outdated.
-    # To prevent this, we get the first word that has not been marked as used.
-    word = driver.execute_script(
-    """
-        word = document.querySelector(".word:not([used])");
-        word.setAttribute("used", "");
-        return word.textContent;
-    """)
-    input_element.send_keys(word + Keys.SPACE)
-
 def main():
+    driver = webdriver.Chrome()
     driver.get("https://monkeytype.com/")
     input_element = driver.find_element(By.ID, "wordsInput")
 
     try:
         while True:
-            type_current_word(input_element)
-            
+            # When getting the word list, the website sometimes changes it,
+            # so the list we have may become outdated.
+            # To prevent this, we get the first word that has not been marked as used.
+            word = driver.execute_script(
+            """
+                word = document.querySelector(".word:not([used])");
+                word.setAttribute("used", "");
+                return word.textContent;
+            """)
+            word += ' '
+            input_element.send_keys(word)        
     except:
         pass
 
